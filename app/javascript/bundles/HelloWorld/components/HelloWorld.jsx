63d98940b1636951a98734028cefd074050d46a5
Evan Morris <evanm978@gmail.com>
2023-04-16 13:40:55 -0700

    one

---
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import style from './HelloWorld.module.css';
+
+const HelloWorld = ({ name, updateName }) => (
+  <div>
+    <h3>
+      Hello,
+      {name}!
+    </h3>
+    <hr />
+    <form>
+      <label className={style.bright} htmlFor="name">
+        Say hello to:
+        <input id="name" type="text" value={name} onChange={(e) => updateName(e.target.value)} />
+      </label>
+    </form>
+  </div>
+);
+
+HelloWorld.propTypes = {
+  name: PropTypes.string.isRequired,
+  updateName: PropTypes.func.isRequired,
+};
+
+export default HelloWorld;
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  get 'hello_world', to: 'hello_world#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
