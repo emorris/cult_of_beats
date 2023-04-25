41b9f2c903a189fa6e4fa22a4c0f73ab5fb4cf42
Evan Morris <evanm978@gmail.com>
2023-04-25 15:13:46 -0700

    base

--- app/javascript/bundles/App/components/UserSettings/Profile/Info.js
+++ app/javascript/bundles/App/components/UserSettings/Profile/Info.js
@@ -2,16 +2,48 @@
 import React from 'react'
 import CardLayout from '../CardLayout'
 
+const basicProfileInfoInputs =[
+  {
+    name: "Artist Name",
+    nameId: "name"
+  },
+  {
+    name: "Url Path Name",
+    nameId: "path_name"
+  }
+]
 
 
-export default function Info({onChange, isLoading, updateProfile}) {
+export default function Info({onChange, isLoading}) {
   
+  const updateProfile = () => {
+    
+  }
+  const inputFields = () => {
+    return basicProfileInfoInputs.map((item) => {
+      return (        
+        <div class="form-control flex" key={`profileInfo-${item.nameId}`}>
+          <label className="label">
+            <span className="label-text  text-neutral-content">{item.name}</span>
+          </label>
+          <input 
+            onChange={onChange}
+            name={item.nameId}
+            type="text" 
+            placeholder="" 
+            className="input input-bordered"
+          />
+        </div>
+      )
+    })
+  }
 
   return (
     <CardLayout title={"Content Links"} isLoading={isLoading} onClick={updateProfile}>
-     
+     {inputFields()}
      <div class="form-control">
         <span className='w-16 place-content-center'>Bio</span>
+          
           <textarea 
             placeholder="Bio" 
             className="textarea textarea-bordered textarea-sm w-full max-w-xs" >
