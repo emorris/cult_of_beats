afef02281b3d30b675800e9fc7a97a74b22566ec
Evan Morris <evanm978@gmail.com>
2023-04-30 16:05:46 -0700

    improvement

--- spec/requests/api/curremt_user_spec.rb
+++ spec/requests/api/curremt_user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Api::CurrentUser", type: :request do
+  before(:each) do
+    @user = FactoryBot.create(:user)
+  end
+  after(:each) do
+    sign_out @user
+  end
+  
   describe "GET /api/current_user" do
     
     it "can not get current_user when user is not logged in" do
@@ -9,10 +16,11 @@ RSpec.describe "Api::CurrentUser", type: :request do
     end
     
     it "it can get current_user when logged in" do 
-      user = FactoryBot.create(:user)
-      sign_in user
+      sign_in @user
       get "/api/current_user"
       expect(response).to be_successful
+      res = JSON.parse(response.body)
+      expect(res['data']['id'].to_i).to eq(@user.id)
     end
 
   end
