3234a9e93475466bfca33de468d9d5c024a5d4aa
Evan Morris <evanm978@gmail.com>
2023-05-08 17:03:56 -0700

    assoications

--- app/models/shared_content.rb
+++ app/models/shared_content.rb
@@ -1,3 +1,5 @@
 class SharedContent < ApplicationRecord
   belongs_to :user
+  belongs_to :user_profile_shared_content
+  has_one :user_profile, through: :user_profile_shared_content
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,7 +2,7 @@ class UserProfile < ApplicationRecord
   include Hashid::Rails
   belongs_to :user
   has_many :user_profile_shared_contents
-  has_many :shared_contents, through: :user_profile_shared_contents
+  has_many :shared_contents, through: :user_profile_shared_cotents
   
   SITE_LINK_TYPES = [
     :instagram, :soundcloud, :youtube, 
